fab68bb81357d125a9e1b2d968de9b1934c31d9e
pcampina <pablo.dsn@gmail.com>
2020-06-13 20:31:27 -0300

    Config dotenv

--- config/database.js
+++ config/database.js
@@ -1,7 +1,22 @@
+require('dotenv').config()
+
 module.exports = {
-  username: 'postgres',
-  password: 'docker',
-  database: 'ccr_bips',
-  host: '127.0.0.1',
-  dialect: 'postgres',
+  development: {
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres'
+  },
+  test: {
+    database: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOSTNAME,
+    dialect: 'postgres'
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres'
+  }
 };
