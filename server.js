604b9195adb1279bcaa2d399dd99f3903d8dd5f8
pcampina <pablo.dsn@gmail.com>
2020-06-13 17:14:22 -0300

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const routes = require("./routes");
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
+
 app.use(routes);
 
 app.listen(3333, () => {
